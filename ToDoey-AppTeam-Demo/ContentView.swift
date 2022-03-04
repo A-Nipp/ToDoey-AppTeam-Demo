512ff62d30b2316c1631be39bbf0cf25a1c0858c
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-03-04 11:01:28 -0500

    initial commit

--- ToDoey-AppTeam-Demo/ContentView.swift
+++ ToDoey-AppTeam-Demo/ContentView.swift
@@ -1,20 +1,64 @@
 //
 //  ContentView.swift
-//  ToDoey-AppTeam-Demo
+//  ToDoey
 //
-//  Created by AlecNipp on 3/4/22.
+//  Created by AlecNipp on 2/28/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentEntry = "" // Holds the String content of the text field
+    @State var todolist: [ToDo] = [] // List of custom data type ToDo
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List {
+                    Section {
+                        TextField("Enter Text", text: $currentEntry, prompt: Text("Enter a task"))
+                            .onSubmit() {
+                                todolist.append(ToDo(content: currentEntry))
+                                currentEntry = ""
+                            }
+                    }
+                    Section {
+                        ForEach($todolist) { $todo in
+                            ToDoView(todo: $todo)
+                        }
+                        
+                    }
+                }
+                .listStyle(.insetGrouped)
+            }
+            .navigationTitle(Text("ToDoey"))
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ToDo: Identifiable {
+    let id = UUID()
+    let content: String
+    var completed = false
+}
+
+struct ToDoView: View {
+    @Binding var todo: ToDo
+    var body: some View {
+        Button {
+            todo.completed.toggle()
+        } label: {
+            HStack {
+                Image(systemName: todo.completed ?  "circle.fill": "circle")
+                    .foregroundColor(.blue)
+                Text(todo.content)
+            }
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+
+struct ToDoey_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
