90f7c25a6cddf4965a9663d645f59e2a825912b3
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-03-04 10:57:50 -0500

    Initial Commit

---
+++ ToDoey-AppTeam-Demo/ToDoey_AppTeam_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoey_AppTeam_DemoApp.swift
+//  ToDoey-AppTeam-Demo
+//
+//  Created by AlecNipp on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoey_AppTeam_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
